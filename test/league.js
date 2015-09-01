4b3292a167645e2aff10b2324c393b01c55e9e59
Jason Pincin <jason@pincin.com>
2015-08-31 20:15:43 -0400

    test invocation

--- test/league.js
+++ test/league.js
@@ -5,20 +5,23 @@ var test   = require('tape'),
     hansa  = require('..')
 
 test('league', function (t) {
-    t.plan(7)
+    t.plan(11)
 
     var service1 = argosy(),
         service2 = argosy(),
         client   = argosy(),
         league   = hansa()
 
-    // .port is a getter that returns a unique stream, allowing league to route vs broadcast
     service1.pipe(league.port()).pipe(service1)
     service2.pipe(league.port()).pipe(service2)
     client.pipe(league.port()).pipe(client)
 
-    service1.accept({ greet: argosy.pattern.match.string })
-    service2.accept({ random: argosy.pattern.match.number })
+    service1.accept({ greet: argosy.pattern.match.string }).process(function (msg, cb) {
+        cb(null, 'Greetings ' + msg.greet)
+    })
+    service2.accept({ max: argosy.pattern.match.array }).process(function (msg, cb) {
+        cb(null, Math.max.apply(null, msg.max))
+    })
 
     league.syncStateChange(function (state) {
         if (state.syncPending) return
@@ -29,12 +32,21 @@ test('league', function (t) {
             })
         }), 'greet pattern exists')
         t.ok(find(league.services, function (svc) {
-            return equal(svc.pattern, { random: argosy.pattern.match.number }) && find(svc.providers, function (provider) {
+            return equal(svc.pattern, { max: argosy.pattern.match.array }) && find(svc.providers, function (provider) {
                 return provider.remoteId === service2.id
             })
         }), 'number pattern exists')
         t.equal(league.services.length, 2, 'should contain two services')
         t.equal(league.ports.length, 3, 'ports should contain all active ports')
+
+        client.invoke({ greet: 'Gege' }, function (err, msg) {
+            t.false(err, 'greet does not result in error')
+            t.equal(msg, 'Greetings Gege', 'greet replies with expected message')
+        })
+        client.invoke({ max: [5, 1, 9, 7] }, function (err, max) {
+            t.false(err, 'max does not result in error')
+            t.equal(max, 9, 'max replies with highest number')
+        })
     })
 
     league.endpointAdded(function (endpoint) {
