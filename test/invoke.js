d0ecc017202646f20ee49c27b50da6fc006a02c8
Jason Pincin <jason@pincin.com>
2015-09-15 09:24:22 -0400

    test more invocation scenarios

---
+++ test/invoke.js
@@ -0,0 +1,28 @@
+var test   = require('tape'),
+    find   = require('array-find'),
+    equal  = require('deep-equal'),
+    argosy = require('argosy'),
+    hansa  = require('..')
+
+test('league', function (t) {
+    t.plan(12)
+
+    var service1 = argosy(),
+        client   = argosy(),
+        league   = hansa()
+
+    service1.pipe(league.port()).pipe(service1)
+    client.pipe(league.port()).pipe(client)
+
+    service1.accept({ greet: argosy.pattern.match.string }).process(function (msg, cb) {
+        cb(null, 'Greetings ' + msg.greet)
+    })
+
+    league.ready(function () {
+        t.ok(find(league.services, function (svc) {
+            return equal(svc.pattern, { greet: argosy.pattern.match.string }) && find(svc.providers, function (provider) {
+                return provider.remoteId === service1.id
+            })
+        }), 'greet pattern exists')
+    })
+})
