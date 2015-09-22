81404d8cc9b9f05a43f40c4ad45c7bf03cf16f17
Jason Pincin <jason@pincin.com>
2015-09-22 15:27:56 -0400

    updated test to reflect new api

--- test/port-error.js
+++ test/port-error.js
@@ -3,28 +3,24 @@ var test    = require('tape'),
     argosy  = require('argosy'),
     hansa   = require('..')
 
-test('league port subscribe error', function (t) {
-    t.plan(2)
+test('league stream subscribe error', function (t) {
+    t.plan(1)
 
     var service1 = argosy(),
         league   = hansa(),
-        port     = league.port()
+        stream   = league.createStream()
 
     var error = new Error('stubbed error')
-    port.subscribeRemote = function () {
+    stream.subscribeRemote = function () {
         return Promise.reject(error)
     }
 
     service1.accept({ greet: argosy.pattern.match.string }).process(function (msg, cb) {
         cb(null, 'Greetings ' + msg.greet)
     })
-    service1.pipe(port).pipe(service1)
+    service1.pipe(stream).pipe(service1)
 
-    league.error(function (err) {
+    league.connectFailed(function (err) {
         t.equal(err, error, 'should produce error on league.error')
-    })
-
-    league.syncStateChange(function (state) {
-        t.deepEqual(state, {syncPending: 0, syncComplete: 0, error: error}, 'syncStateChange should include error')
     })
 })
