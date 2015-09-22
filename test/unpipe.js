659b2f4ed9def785c508453d06165a7e074d560d
Jason Pincin <jason@pincin.com>
2015-09-22 15:28:08 -0400

    updated test to reflect new api

--- test/unpipe.js
+++ test/unpipe.js
@@ -3,31 +3,22 @@ var test   = require('tape'),
     hansa  = require('..')
 
 test('unpipe', function (t) {
-    t.plan(4)
+    t.plan(3)
 
     var service1 = argosy(),
-        service2 = argosy(),
         league   = hansa(),
-        port1    = league.port(),
-        port2    = league.port()
+        stream1  = league.createStream()
 
-    service1.pipe(port1).pipe(service1)
-    service2.pipe(port2).pipe(service2)
+    service1.pipe(stream1).pipe(service1)
 
     service1.accept({ help: argosy.pattern.match.defined })
-    service2.accept({ help: argosy.pattern.match.defined })
-    league.ready(function () {
+    league.connected(function () {
         t.equal(league.services.length, 1, 'league exposes 1 service before unpipe')
-        t.equal(league.services[0].providers.length, 2, 'with 2 providers before unpipe')
-        service1.unpipe(port1)
+        t.equal(league.services[0].providers.length, 1, 'with 1 provider before unpipe')
+        service1.unpipe(stream1)
 
-        league.ready(function () {
-            t.equal(league.services[0].providers.length, 1, 'with 1 provider after 1st unpipe')
-            service2.unpipe(port2)
-
-            league.ready(function () {
-                t.equal(league.services.length, 0, 'with 0 services after 2nd unpipe')
-            })
+        league.disconnected(function () {
+            t.equal(league.services.length, 0, 'with 0 providers after unpipe')
         })
     })
 })
