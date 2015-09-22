685d1ab8e4d8f8a6eeee658465dc5001489b3954
Jason Pincin <jason@pincin.com>
2015-09-22 15:26:27 -0400

    large api refactor, more consistent/reasonable naming

--- index.js
+++ index.js
@@ -5,101 +5,107 @@ var argosy         = require('argosy'),
     after          = require('afterward'),
     Promise        = require('promise-polyfill'),
     find           = require('array-find'),
+    assign         = require('object-assign'),
     createRegistry = require('./lib/registry'),
     setImmediate   = require('timers').setImmediate
 
 module.exports = function createLeague () {
-    var registry     = createRegistry(),
-        members      = []
+    var registry     = createRegistry()
 
     var league = Object.defineProperties({}, {
-        id             : { value: uuid(), enumerable: true },
-        ports          : { value: [], enumerable: true },
-        patterns       : { get: getPatterns, enumerable: true },
-        services       : { get: getServices, enumerable: true },
-        connect        : { value: connect, configurable: true },
-        disconnect     : { value: disconnect, configurable: true },
-        port           : { value: createPort, configurable: true },
-        error          : { value: eventuate({ requireConsumption: true }), configurable: true },
-        endpointAdded  : { value: eventuate(), configurable: true },
-        endpointRemoved: { value: eventuate(), configurable: true }
+        id           : { value: uuid(), enumerable: true },
+        members      : { value: [], enumerable: true, writable: true },
+        patterns     : { get: getPatterns, enumerable: true },
+        services     : { get: getServices, enumerable: true },
+        connect      : { value: connect, configurable: true },
+        disconnect   : { value: disconnect, configurable: true },
+        createStream : { value: createStream, configurable: true },
+        error        : { value: eventuate({ requireConsumption: true }), configurable: true },
+        connected    : { value: eventuate(), configurable: true },
+        connectFailed: { value: eventuate(), configurable: true },
+        disconnected : { value: eventuate(), configurable: true }
     })
 
     return league
 
-    // hansa league terminology - a port is just a local argosy endpoint
-    // each "port" will share it's id with the league so that all the remote
-    // argosy endpoints (one connected to each port), see's the same id and services
+    // hansa league terminology - a member is a remote (same process or different) argosy-like endpoint
+    // connected to the league. These remote argosy endpoints are paired with local argosy endpoints created
+    // via createStream. All local argosy endpoints have the same ID and expose all service patterns that the
+    // league as a whole can satisfy
 
-    function connect (endpoint, cb) {
-        if (Array.isArray(endpoint)) return after(Promise.all(endpoint.map(function (singleEndpoint) {
-            return connect(singleEndpoint)
+    function connect (remote, cb) {
+        if (Array.isArray(remote)) return after(Promise.all(remote.map(function eachRemote (remote) {
+            return connect(remote)
         })), cb)
-        var existingMember = find(members, connectedVia(endpoint))
+
+        var existingMember = find(league.members, hasRemote(remote))
         if (existingMember) return after(Promise.resolve(existingMember), cb)
-        var port = createPort()
-        endpoint.pipe(port).pipe(endpoint)
-        return after(once(league.endpointAdded, function (member) {
-            return member.endpoint === endpoint
-        }), cb)
-    }
+        remote.pipe(league.createStream()).pipe(remote)
 
-    function disconnect (endpoint, cb) {
-        var existingMember = find(members, connectedVia(endpoint))
-        if (!existingMember) return after(Promise.resolve(), cb)
-        members.filter(connectedVia(endpoint)).forEach(function (member) {
-            member.endpoint.unpipe(member.port)
+        var done = new Promise(function onConnectionResult (resolve, reject) {
+            once.match(league.connected, hasRemote(remote), resolve)
+            once.match(league.connectFailed, hasRemote(remote), reject)
         })
-        return after(once(league.endpointRemoved, function (member) {
-            return member.endpoint === endpoint
-        }), cb)
+        return after(done, cb)
     }
 
-    function createPort () {
-        var port = argosy({ id: league.id })
-        league.ports.push(port)
+    function disconnect (remote, cb) {
+        if (Array.isArray(remote)) return after(Promise.all(remote.map(function eachRemote (remote) {
+            return disconnect(remote)
+        })), cb)
+
+        var existingMember = find(league.members, hasRemote(remote))
+        if (!existingMember) return after(Promise.resolve(), cb)
+        league.members.filter(hasRemote(remote)).forEach(function (member) {
+            member.remote.unpipe(member)
+        })
+        return after(once.match(league.disconnected, hasRemote(remote)), cb)
+    }
+
+    function createStream () {
+        var stream = argosy({ id: league.id })
 
         function routePattern (pattern) {
-            port.accept(pattern).process(route)
+            stream.accept(pattern).process(route)
         }
         registry.patterns.forEach(routePattern)
         registry.patternAdded(routePattern)
 
-        port.on('pipe', function (endpoint) {
-            port.subscribeRemote(['services']).then(function () {
-                var member = { endpoint: endpoint, port: port }
-                members.push(member)
-                league.endpointAdded.produce(member)
+        stream.on('pipe', function (remote) {
+            assign(stream, { remote: remote })
+            league.members.push(stream)
+            stream.subscribeRemote(['services']).then(function () {
+                setImmediate(league.connected.produce, stream)
             }).catch(function (err) {
-                league.error.produce(err)
+                league.members.splice(league.members.indexOf(stream), 1)
+                setImmediate(league.connectFailed.produce, assign(err, { stream: stream }))
             })
         })
-        port.on('unpipe', clean)
-        port.remoteServiceAdded(onRemoteService)
+        stream.on('unpipe', clean)
+        stream.remoteServiceAdded(onRemoteService)
 
-        function clean (endpoint) {
-            port.unpipe()
-            port.removeAllListeners()
-            port.serviceAdded.removeAllConsumers()
-            var remoteServices = port.services.filter(function (svc) {
+        function clean (remote) {
+            var stream = find(league.members, hasRemote(remote))
+            stream.unpipe()
+            stream.removeAllListeners()
+            stream.remoteServiceAdded.removeAllConsumers()
+            var remoteServices = stream.services.filter(function (svc) {
                 return svc.remote
             })
             remoteServices.forEach(function (svc) {
-                registry.remove(svc, port)
+                registry.remove(svc, stream)
             })
-            league.ports.splice(league.ports.indexOf(port), 1)
-            members = members.filter(function filterUnpipedPort (member) {
-                return member.port !== port
+            league.members = league.members.filter(function filterUnpiped (stream) {
+                return stream.remote !== remote
             })
-            setImmediate(league.endpointRemoved.produce, {
-                endpoint: endpoint,
-                port    : port
-            })
+            setImmediate(league.disconnected.produce, stream)
         }
+
         function onRemoteService (svc) {
-            registry.add(svc, port)
+            registry.add(svc, stream)
         }
-        return port
+
+        return stream
     }
 
     function route (msg, cb) {
@@ -115,8 +121,8 @@ module.exports = function createLeague () {
     }
 }
 
-function connectedVia (endpoint) {
-    return function memberConnectedVia (member) {
-        return member.endpoint === endpoint
+function hasRemote (remote) {
+    return function hasRemote (member) {
+        return member.remote === remote
     }
 }
